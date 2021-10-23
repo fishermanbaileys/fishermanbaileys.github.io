8ad9c5599148f7535e7eaa165236a26ecf7ba124
Bailey Graham <87393664+fishermanbaileys@users.noreply.github.com>
2021-10-22 21:22:17 -0500

    Nav update

--- Underconstruction/Website/js/javascript.js
+++ Underconstruction/Website/js/javascript.js
@@ -70,6 +70,22 @@ $(function()
    {
     $(".menuitem").removeClass("active");
     $(this).addClass("active");
+
+  
   });
 });
+
+$('.menuitem[href*="#"]').on('click', function (e) {
+  e.preventDefault()
+
+  $('html, body').animate(
+    {
+      scrollTop: $($(this).attr('href')).offset().top,
+    },
+    500,
+    'linear'
+  )
+})
+
+
 }
